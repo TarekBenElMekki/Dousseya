8c08e0d3eb59196f06b79422ff3cdb1e20b5a44e
TarekBenElMekki <116441045+TarekBenElMekki@users.noreply.github.com>
2023-12-03 09:55:55 +0000

    Adding The Home Page

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask 
+from flask import Flask , render_template
 app = Flask(__name__)
 @app.route("/")
-def hello_world () :
-  return " Hello World "
+def html () :
+  return render_template("Home.html")
 
 if __name__ == "__main__" :
   app.run(host="0.0.0.0", port=8080)
